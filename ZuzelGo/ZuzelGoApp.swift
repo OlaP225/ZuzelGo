a2eb7567dfa8708a90b89e8f93f4c2b541e7c140
Ola <aleksandra.plichta@poczta.fm>
2026-01-28 17:53:36 +0100

    Initial Commit

---
+++ ZuzelGo/ZuzelGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZuzelGoApp.swift
+//  ZuzelGo
+//
+//  Created by Aleksandra Plichta on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ZuzelGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
