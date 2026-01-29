863017fbdd138b43ff99c8a29081b193a21c7cd1
Ola <aleksandra.plichta@poczta.fm>
2026-01-29 15:41:18 +0100

    REFACTOR Little refactor of background and structure of  first view.

--- ZuzelGo/ContentView.swift
+++ ZuzelGo/ContentView.swift
@@ -9,63 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack {
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Spacer()
-                Text("ŻużelGo")
-                    .font(.system(.largeTitle, design: .monospaced))
-                    .foregroundColor(Color.white)
-                Spacer()
-            }
-            Spacer()
-            Spacer()
-            Image("transport")
-                .resizable()
-                .frame(width: 100, height: 100)
-            Spacer()
-            Text("Odkryj żużel!")
-                .font(.system(.largeTitle, design: .monospaced))
-                .fontWeight(.bold)
-                .foregroundColor(Color.white)
-            Spacer()
-            Text("Rywalizuj ze znajomymi, obstawiaj biegi i baw się!")
-                .font(.system(.body, design: .monospaced))
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
-                .frame(width: 300)
-                .foregroundStyle(Color(hue: 1.0, saturation: 0.0, brightness: 0.824))
-            Spacer()
-            Spacer()
+        
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color.black, Color.gray]),
+                           startPoint: .bottomTrailing,
+                           endPoint: .topLeading)
+                .ignoresSafeArea(.all)
 
-            Button(action: guestEnter){
-                Text("Dołącz jako gość")
-                    .foregroundColor(Color.black)
-                    .font(.system(.headline, design: .monospaced))
-                    .fontWeight(.bold)
-            }
-            .frame(width: UIScreen.main.bounds.width * 0.9, height: 50, alignment: .center)
-            .background(Color(hue: 0.04, saturation: 0.808, brightness: 1.0))
-            .cornerRadius(15)
+            VStack {
+                HStack {
+                    Spacer(minLength: 200)
+                    Text("ŻużelGo")
+                        .font(.system(.largeTitle, design: .monospaced))
+                        .foregroundColor(Color.white)
+                    Spacer(minLength: 10)
+                }
+                Spacer()
+                Spacer()
+                Image("transport")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                Spacer()
+                VStack(spacing: 10) {
+                    Text("Odkryj żużel!")
+                        .font(.system(.largeTitle, design: .monospaced))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                    Text("Rywalizuj ze znajomymi, obstawiaj biegi i baw się!")
+                        .font(.system(.body, design: .monospaced))
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 300)
+                        .foregroundStyle(Color(hue: 1.0,
+                                               saturation: 0.0,
+                                               brightness: 0.824))
+                }
+                Spacer()
 
-            Button(action: memberEnter){
-                Text("Zaloguj/Zarejestruj się")
-                    .foregroundColor(Color.black)
-                    .font(.system(.headline, design: .monospaced))
-                    .fontWeight(.bold)
+
+                Button(action: guestEnter){
+                    Text("Dołącz jako gość")
+                        .foregroundColor(Color.black)
+                        .font(.system(.headline, design: .monospaced))
+                        .fontWeight(.bold)
+                }
+                .frame(width: UIScreen.main.bounds.width * 0.9, height: 50, alignment: .center)
+                .background(Color(hue: 0.04, saturation: 0.808, brightness: 1.0))
+                .cornerRadius(15)
+
+                Button(action: memberEnter){
+                    Text("Zaloguj/Zarejestruj się")
+                        .foregroundColor(Color.black)
+                        .font(.system(.headline, design: .monospaced))
+                        .fontWeight(.bold)
+                }
+                .frame(width: UIScreen.main.bounds.width * 0.9, height: 50, alignment: .center)
+                .background(Color.white)
+                .cornerRadius(15)
+                Spacer()
+                Spacer()
             }
-            .frame(width: UIScreen.main.bounds.width * 0.9, height: 50, alignment: .center)
-            .background(Color.white)
-            .cornerRadius(15)
-            Spacer()
-            Spacer()
+            .padding()
+            
         }
-        .padding()
-        .background(Color(hue: 1.0, saturation: 0.0, brightness: 0.154))
     }
 }
 
